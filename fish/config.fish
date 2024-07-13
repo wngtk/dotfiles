948a3a99407d8b72402c3eb1fa63064742a41c21
Wang Tiaoke <fnmain.rs@gmail.com>
2024-07-13 22:41:53 +0800

    Indent using tabs

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,19 @@
 abbr -a e nvim
+abbr -a m make
+abbr -a g git
 
-set -x MANPAGER "nvim +Man!"
+if command -v eza > /dev/null
+	abbr -a l 'eza'
+	abbr -a ls 'eza'
+	abbr -a ll 'eza -l'
+	abbr -a lll 'eza -la'
+end
+
+# set -x MANPAGER "nvim +Man!"
 fish_add_path -gp "$HOME/.dotfiles/bin" "$HOME/.local/bin"
 
 if status --is-interactive
-    set fish_greeting
+	set fish_greeting
 end
 
 if test -f /home/wngtk/.autojump/share/autojump/autojump.fish
