93daa491bcd6e61a70ceabdce33558cc3ed2a946
Wang Tiaoke <wngtk@outlook.com>
2024-07-29 09:30:19 +0800

    make pwsh history likes fish

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,7 @@
 Import-Module PSReadLine
 Set-PSReadLineOption -EditMode Emacs
+
+Set-PSReadLineOption -AddToHistoryHandler {
+        param([string]$line)
+        return $line.Length -gt 3 -and $line[0] -ne ' ' -and $line[0] -ne ';'
+}
