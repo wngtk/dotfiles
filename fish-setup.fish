8947c8da5a4b953633ec8ba87fb5c1bd583b6ecd
Wang Tiaoke <wngtk@outlook.com>
2024-08-08 15:35:43 +0800

    added fish-setup.fish

---
+++ fish-setup.fish
@@ -0,0 +1,12 @@
+#!/usr/bin/env fish
+
+if not command -v fisher > /dev/null
+    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+end
+
+if not command -v nvm > /dev/null
+    fisher install jorgebucaran/nvm.fish
+end
+
+nvm install lts
+set --universal nvm_default_version lts
