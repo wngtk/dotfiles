410b9924782705d10d253c4a46f96e4fd4c5c88f
Wang Tiaoke <wngtk@outlook.com>
2024-10-11 16:17:08 +0800

    catppuccin already supports lualine.nvim

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,10 +2,5 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	lazy = false, -- also load at start since it's UI
-    enabled = false,
-	opts = {
-		options = {
-			theme = "dracula",
-		},
-	},
+	opts = {},
 }
