2a175774acde174beb07dc3e8517fa74691361b4
Tony Wang <wngtk@outlook.com>
2025-10-29 13:25:30 +0800

    Customize fish_prompt

--- fish/config.fish
+++ fish/config.fish
@@ -43,3 +43,28 @@ function d
 		cd ..
 	end
 end
+
+# Fish git prompt
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate ''
+set __fish_git_prompt_showupstream 'none'
+set -g fish_prompt_pwd_dir_length 3
+
+function fish_prompt
+	set_color brblack
+	echo -n "["(date "+%H:%M")"] "
+	set_color blue
+	echo -n (command -q hostname; and hostname; or hostnamectl hostname)
+	if [ $PWD != $HOME ]
+		set_color brblack
+		echo -n ':'
+		set_color yellow
+		echo -n (basename $PWD)
+	end
+	set_color green
+	printf '%s ' (fish_git_prompt)
+	set_color red
+	echo -n '| '
+	set_color normal
+end
