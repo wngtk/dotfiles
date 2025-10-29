caba2b57c6374b5eb632daeb1f9e343eee2ca7cb
Tony Wang <wngtk@outlook.com>
2025-10-29 14:58:50 +0800

    Update install-neovim.sh

--- install-neovim.sh
+++ install-neovim.sh
@@ -2,7 +2,15 @@
 
 set -e
 
+TARGET_BRANCH=release-0.11
+DEST_DIR=neovim-${TARGET_BRANCH}
+
 cd /tmp
-git clone -b release-0.10 https://github.com/neovim/neovim.git --depth=1
-cd neovim
+if [[ ! -d $DEST_DIR ]]; then
+    git clone -b ${TARGET_BRANCH} https://github.com/neovim/neovim.git --depth=1 $DEST_DIR
+fi
+
+cd $DEST_DIR
+git pull
+
 make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX=$HOME/.neovim install
